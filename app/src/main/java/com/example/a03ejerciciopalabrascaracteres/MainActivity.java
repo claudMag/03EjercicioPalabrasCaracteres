5373ffae6794322a0d1e603e678695212ccad53d
claudiamaggio <claudia.maggio1@gmail.com>
2022-10-03 13:26:10 +0200

    corretto

--- app/src/main/java/com/example/a03ejerciciopalabrascaracteres/MainActivity.java
+++ app/src/main/java/com/example/a03ejerciciopalabrascaracteres/MainActivity.java
@@ -1,9 +1,5 @@
 package com.example.a03ejerciciopalabrascaracteres;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,12 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText txtFrase;
     private Button btnPalabras;
     private Button btnCaracteres;
+
+    //CONSTANTES
+    public static final int PALABRAS = 1;
+    public static final int CARACTERES = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,35 +26,70 @@ public class MainActivity extends AppCompatActivity {
 
         inicializaVistas();
 
+        //inicializaEventos();
+    }
+
+    private void inicializaEventos() {
         btnPalabras.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String frase = txtFrase.getText().toString();
-                Intent intent = new Intent(MainActivity.this, PalabraActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("PALABRAS", frase);
-                intent.putExtras(bundle);
-                startActivity(intent);
+                onclick(PALABRAS);
+
             }
         });
 
         btnCaracteres.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String frase = txtFrase.getText().toString();
-                Intent intent = new Intent(MainActivity.this, PalabraActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("CARACTERES", frase);
-                intent.putExtras(bundle);
-                startActivity(intent);
+                onclick(CARACTERES);
             }
         });
     }
 
+    public void onClickBotones(View boton){
+        //añado esta funcion en la interfaz donde onclick
+
+        String frase = txtFrase.getText().toString();
+        if(!frase.isEmpty()){
+            Bundle bundle = new Bundle();
+            bundle.putString("FRASE", frase);
+            //Cada boton inserta su ID
+            bundle.putInt("OPERACION", boton.getId());
+            Intent intent = new Intent(MainActivity.this, PalabraActivity.class);
+            intent.putExtras(bundle);
+            startActivity(intent);
+
+        }
+        else{
+            Toast.makeText(MainActivity.this, "Frase vacía", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 
     private void inicializaVistas() {
         txtFrase = findViewById(R.id.txtFraseMain);
         btnPalabras = findViewById(R.id.btnPalabrasMain);
         btnCaracteres = findViewById(R.id.btnCaracteresMain);
     }
+
+
+    private void onclick(int PALABRAS) {
+        String frase = txtFrase.getText().toString();
+        if(!frase.isEmpty()){
+            Bundle bundle = new Bundle();
+            bundle.putString("FRASE", frase);
+            bundle.putInt("OPERACION", PALABRAS);
+            Intent intent = new Intent(MainActivity.this, PalabraActivity.class);
+            intent.putExtras(bundle);
+            startActivity(intent);
+
+        }
+        else{
+            Toast.makeText(MainActivity.this, "Frase vacía", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/a03ejerciciopalabrascaracteres/PalabraActivity.java
+++ app/src/main/java/com/example/a03ejerciciopalabrascaracteres/PalabraActivity.java
@@ -9,10 +9,11 @@ import android.widget.Toast;
 
 import java.util.IllegalFormatCodePointException;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 public class PalabraActivity extends AppCompatActivity {
 
-    private EditText txtResultado;
+    private EditText txtResultado; //ha usato un textview
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,26 +21,42 @@ public class PalabraActivity extends AppCompatActivity {
 
         inicializaVistas();
 
+        //Obtener informacion enviada
         Intent intent = this.getIntent();
         Bundle bundle = intent.getExtras();
 
-
         if (bundle != null){
-            if (bundle.containsKey("PALABRAS")){
-                String frase = bundle.getString("PALABRAS");
-                String[] palabras = frase.split(" ");
-                txtResultado.setText("NÚMERO PALABRAS: "+palabras.length);
-
+            String frase = bundle.getString("FRASE");
+            int operacion = bundle.getInt("OPERACION");
+            //if (operacion == MainActivity.PALABRAS){
+            if (operacion == R.id.btnPalabrasMain){
+                int resultado = contarPalabras(frase);
+                txtResultado.setText("Palabras: "+resultado);
             }
-            if (bundle.containsKey("CARACTERES")){
-                String frase = bundle.getString("CARACTERES");
-                txtResultado.setText("NÚMERO CARACTERES: "+frase.length());
+            else if(operacion == R.id.btnCaracteresMain){
+               int resultado = contarCaracteres(frase);
+               txtResultado.setText("Caracteres: "+resultado);
             }
-
+            else{
+                txtResultado.setText("ERROR OPERACIÓN");
+            }
         }
     }
 
+    private int contarCaracteres(String frase) {
+        return frase.length();
+    }
+
+    private int contarPalabras(String frase) {
+        //elimina todos los tokens que se quedan vacios, por defecto el separador es el espacio
+        //si no seria new StringTokenizer(frase, "-");
+        StringTokenizer stringTokenizer = new StringTokenizer(frase);
+
+        return stringTokenizer.countTokens();
+    }
+
     private void inicializaVistas() {
+
         txtResultado = findViewById(R.id.txtResultadoPalabra);
     }
 }
